79f83e875dadb0e72146bc430db9b93692f80cf0
BananaNotSplit <98235195+BananaNotSplit@users.noreply.github.com>
2026-03-02 09:32:20 -0500

    Added automatic status changing.

--- src/Modules/StatusModule.ts
+++ src/Modules/StatusModule.ts
@@ -1,16 +1,26 @@
 import { Client } from "discord.js";
 import EntangledModule from "../ModuleSystem/EntangledModule";
 
+export interface IRefreshTime {
+	hours: number
+	minutes: number
+	seconds: number
+}
+
 export interface IStatusModuleConfiguration {
 	statuses: string[]
+	refresh: IRefreshTime
 }
 
 export class StatusModule extends EntangledModule<IStatusModuleConfiguration> {
+	statusTimeout: NodeJS.Timeout|null = null
+
 	NewData(): IStatusModuleConfiguration {
 		return {
 			statuses: [
 				"edit StatusModule.json"
-			]
+			],
+			refresh: { hours: 5, minutes: 0, seconds: 0 }
 		}
 	}
 
@@ -27,7 +37,11 @@ export class StatusModule extends EntangledModule<IStatusModuleConfiguration> {
 	}
 
 	Ready(): void {
+		if (!this.data) return
 		this.SetRandomStatus()
+		this.statusTimeout = setInterval(() => this.SetRandomStatus(),
+		(this.data?.refresh.seconds + (this.data?.refresh.minutes + this.data?.refresh.hours * 60) * 60) * 1000
+	)
 	}
 
 	Cleanup(): void {
